8e527bc61dc4375df1d5e8649bfc478ebc5df75a
Angel Rivera <ariv3ra@gmail.com>
2023-01-29 16:32:25 -0500

    fixed the tf block that were broke

--- terraform/digital_ocean/tfcloud/main.tf
+++ terraform/digital_ocean/tfcloud/main.tf
@@ -18,14 +18,14 @@ resource "tfe_organization" "org" {
 
 resource "tfe_workspace" "ws_iac_do_k8s" {
   name         = var.workspace_name
-  organization = var.org_name
+  organization = tfe_organization.org.name
   execution_mode = var.execution_mode
   tag_names    = ["k8s_cluster", "cicd-workshop"]
 }
 
 resource "tfe_workspace" "ws_do_k8s_deploy" {
-  name         = "${var.workspace_name}-deployment"
-  organization = var.org_name
-  execution_mode = var.execution_mode
+  name         = "${tfe_workspace.ws_iac_do_k8s.name}-deployment"
+  organization = tfe_organization.org.name
+  execution_mode = tfe_workspace.ws_iac_do_k8s.execution_mode
   tag_names    = ["k8s_deployment", "cicd-workshop-app"]
 }
